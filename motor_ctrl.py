84574e2b28dc3e86a15ab61dda9aaaa4ce04cbdc
dBerriff <berriff@icloud.com>
2024-01-08 21:31:59 +0000

    Add kick_start() method to motor_ctrl.py

--- motor_ctrl.py
+++ motor_ctrl.py
@@ -27,7 +27,7 @@ class MotorCtrl:
         else:
             return 0
 
-    def __init__(self, channel, name, min_pc=0):
+    def __init__(self, channel, name, min_pc):
         self.channel = channel
         self.name = name  # for print or logging
         self.min_u16 = self.pc_u16(min_pc)  # start-up speed
@@ -49,21 +49,26 @@ class MotorCtrl:
         self.channel.set_dc_u16(dc_u16)
         self.speed_u16 = dc_u16
 
-    def kick_start(self):
+    def rotate_pc(self, dc_pc):
+        """ rotate motor in state direction at u16 duty cycle """
+        self.rotate_u16(self.pc_u16(dc_pc))
+
+    async def kick_start(self, kick_ms=10):
         """ send pulses to start a motor """
         if self.state in self.run_set:
             self.channel.set_dc_u16(self.KICK_U16)
-            time.sleep_us(100)
-            self.channel.set_dc_u16(self.min_u16)
+            await asyncio.sleep_ms(kick_ms)
+            self.rotate_u16(self.min_u16)
 
-    async def accel_u16(self, target_pc, period_ms=1_000):
+    async def accel_pc(self, target_pc, period_ms=1_000):
         """ accelerate from current to target speed in trans_period_ms
             - supports (target < current) for deceleration
         """
-        # consider passing period as parameter
         if self.state in self.run_set:
-            n_steps = 25
             target_u16 = self.pc_u16(target_pc)
+            if target_u16 < self.min_u16:
+                target_u16 = 0
+            n_steps = 25
             # check for start from rest
             if self.speed_u16 == 0 and target_u16 > 0:
                 self.speed_u16 = self.min_u16
@@ -99,20 +104,26 @@ async def main():
         'bridge_pins': (4, 5, 6, 7),
         'run_btn': 20,
         'kill_btn': 22,
-        'pulse_f': 500,
-        'motor_start_pc': 30,
+        'pulse_f': 20_000,
+        'motor_min_pc': 5,
         'motor_a_speed': {'f': 100, 'r': 95},
         'motor_b_speed': {'f': 95, 'r': 95},
         'motor_hold_period': 5
     }
 
     controller = L298N(params['pwm_pins'], params['bridge_pins'], params['pulse_f'])
-    motor_a = MotorCtrl(controller.channel_a, name='A', min_pc=params['motor_start_pc'])
-    motor_b = MotorCtrl(controller.channel_b, name='B', min_pc=params['motor_start_pc'])
-    # set initial state to 'S'
+    motor_a = MotorCtrl(controller.channel_a, name='A', min_pc=params['motor_min_pc'])
+
+    motor_a.set_state('F')
+    await motor_a.kick_start()
+    motor_a.rotate_u16(motor_a.min_u16)
+    await asyncio.sleep(5)
     motor_a.stop()
-    motor_b.stop()
-    motor_a.kick_start()
+    await asyncio.sleep(1)
+    motor_a.set_state('R')
+    await motor_a.kick_start()
+    motor_a.rotate_u16(motor_a.min_u16)
+    await asyncio.sleep(5)
     motor_a.stop()
 
 
